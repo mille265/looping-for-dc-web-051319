fe1739850d9a2e7af6ea7e19fc4ad851812b8775
meghan miller <mille265@gmail.com>
2019-05-07 17:38:51 +0000

    Automatically backed up by Learn

--- spec/for_spec.rb
+++ spec/for_spec.rb
@@ -1,9 +1,8 @@
-require_relative "spec_helper"
-require_relative "../for.rb"
 
-describe "#using_for" do 
-	it "puts until the phrase 'Wingardium Leviosa' 10 times, once for each item on the checklist" do
-		looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-		expect{ using_for }.to output(looping_string).to_stdout
+def using_for
+	checklist = 1..10
+	#your code here
+	for item in checklist
+		puts "Wingardium Leviosa"
 	end
 end
